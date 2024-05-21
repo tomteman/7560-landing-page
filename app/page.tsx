0ebb741f2ae883b5d4303b78a81e6cea79469548
Tom Teman <tomteman42@gmail.com>
2024-05-21 21:08:47 +0300

    nicer layout for subheader

--- app/page.tsx
+++ app/page.tsx
@@ -39,9 +39,11 @@ export default function Home() {
       <div className="relative z-[-1] flex flex-col items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <div className="text-center">
           <h1 className="text-5xl font-extrabold">RIP-7560</h1>
-          <h2 className="mt-4 text-2xl text-gray-700 dark:text-gray-300">
-          An account abstraction proposal that introduces consensus-layer protocol changes, instead of relying on higher-layer infrastructure
-          </h2>
+          <div className="max-w-md lg:max-w-3xl">
+            <h2 className="mt-4 text-2xl text-gray-700 dark:text-gray-300">
+            An account abstraction proposal that introduces consensus-layer protocol changes, instead of relying on higher-layer infrastructure
+            </h2>
+          </div>
         </div>
       </div>
 
