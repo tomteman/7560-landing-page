da5702aab5b179d4d4f3bea11fa64ff82af38116
Tom Teman <tomteman42@gmail.com>
2024-05-21 21:41:17 +0300

    mobile elements adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <p className="hidden lg:flex fixed left-0 top-0 w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           RIP Status:&nbsp;
           <code className="font-mono font-bold">Draft</code>
         </p>
@@ -38,9 +38,9 @@ export default function Home() {
 
       <div className="relative z-[-1] flex flex-col items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <div className="text-center">
-          <h1 className="text-5xl font-extrabold">RIP-7560</h1>
+          <h1 className="text-4xl large:text-5xl font-extrabold">RIP-7560</h1>
           <div className="max-w-md lg:max-w-3xl">
-            <h2 className="mt-4 text-2xl text-gray-700 dark:text-gray-300">
+          <h2 className="mt-4 text-s lg:text-2xl text-gray-700 dark:text-gray-300">
             An account abstraction proposal that introduces consensus-layer protocol changes, instead of relying on higher-layer infrastructure
             </h2>
           </div>
